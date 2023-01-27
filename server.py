1854efaede5f074461fbd3a4b20ea5785cd50ef7
Youssef G Zahar <youssef@infra04-wg012.eduroam.wireless.umn.edu>
2023-01-27 14:09:07 -0600

    refactored to work with server.py

--- server.py
+++ server.py
@@ -2,12 +2,8 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-# @app.route('/hi', methods=['GET'])
-# def hi():
-#   user_name = request.args.get("userName", "unknown")
-#   return render_template('main.html', user=user_name) 
+def main():
+  user_name = request.args.get("userName", "unknown")
+  return render_template('index.html', user=user_name) 
