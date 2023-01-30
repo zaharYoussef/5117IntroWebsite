b242fd5949f1492ca603288882e5016b1cc85d76
Youssef G Zahar <youssef@infra04-wg013.eduroam.wireless.umn.edu>
2023-01-30 14:39:15 -0600

    added flask form

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 from flask import Flask, request, render_template
 
+
 app = Flask(__name__)
 
+
+# messages = [{'yesChoice': 'I am glad this coin was helpful'},
+#             {'noChoice': 'Let the coin guide you, believe in its power'}
+#             ]
 
 @app.route("/")
 def main():
   user_name = request.args.get("userName", "unknown")
   return render_template('index.html', user=user_name) 
+
+@app.route('/form/', methods=('GET', 'POST'))
+def form():
+  if request.method == 'POST':
+    try:
+      choice = request.form['choice']
+      message = "thank you for the feedback"
+    except KeyError:
+      message = "why did you submit without picking a choice??!?"
+    
+    return render_template('index.html', choice = message) 
+          
+  return render_template('form.html')
